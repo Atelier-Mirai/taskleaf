63c2a70bb413e75b73739040ec351494999e7375
Atelier-Mirai <contact@atelier-mirai.net>
2020-11-05 23:27:57 +0900

    route.rbからエラーハンドリング外す

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get    '/signup', to: 'users#new'
 
   # error 表示
-  match '*path' => 'application#error404', via: :all if Rails.env.production?
+  # match '*path' => 'application#error404', via: :all if Rails.env.production?
 
   # letter opener web
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
