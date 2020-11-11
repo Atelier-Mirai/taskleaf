7951615c7b423c26747addf4888f24251ef7e9a1
Atelier-Mirai <contact@atelier-mirai.net>
2020-11-11 23:01:18 +0900

    csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :error
   helper_method :current_user
   before_action :login_required
