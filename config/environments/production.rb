8423dc350d22120e8a72a6730fd4cc36b8a0fcae
Atelier-Mirai <contact@atelier-mirai.net>
2021-02-02 13:25:04 +0900

    SENDGRID

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'heroku.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-      user_name: 'apikey',
-      password: ENV['SENDGRID_APIKEY'],
+      # user_name: 'apikey',
+      # password: ENV['SENDGRID_APIKEY'],
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
       domain: 'heroku.com',
       address: 'smtp.sendgrid.net',
       port: 587,
