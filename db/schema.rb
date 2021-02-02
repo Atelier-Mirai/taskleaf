6c8c846151e0a4eed664393b93ea4e5e7b30efa7
Atelier-Mirai <contact@atelier-mirai.net>
2021-02-02 09:47:17 +0900

    rails active_storage:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_233235) do
+ActiveRecord::Schema.define(version: 2021_02_02_004542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
